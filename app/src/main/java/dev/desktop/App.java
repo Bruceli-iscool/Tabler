11877844608c8ee2b9d0d0cdaf1cb51c1e931b00
Bruce Li <isanoob1897@outlook.com>
2025-02-15 19:49:45 -0800

    add loading screen
    Signed-off-by: Bruce Li <isanoob1897@outlook.com>

--- app/src/main/java/dev/desktop/App.java
+++ app/src/main/java/dev/desktop/App.java
@@ -1,7 +1,24 @@
 package dev.desktop;
+import javax.swing.*;
+import java.awt.*;
 
 public class App {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException{
+        loadingScreen();
     }
+    /*
+    * function for displaying a loading screen
+    *
+    */
+    private static void loadingScreen() throws InterruptedException{
+        JFrame loadingScreen = new JFrame("Tabler v0.0");
+        loadingScreen.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        loadingScreen.setVisible(true);
+        loadingScreen.setSize(400, 500);
+        JLabel logo = new JLabel("Tabler");
+        logo.setFont(new Font("Calibre", Font.BOLD, 56));
+        loadingScreen.add(logo);
+        loadingScreen.setLocation(1100, 500);
+    }
+
 }
